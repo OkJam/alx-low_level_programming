4730f5ada7bff77797f40ac7ae3256037034e44e
James Okwoche <jamesokwocheo@gmail.com>
2022-06-30 23:08:19 +0000

    add: : 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
-* main - Entry point
-*/
-int main (void)
+/**
+ * Entry point 
+ */
+int main ()
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
     return 0;
