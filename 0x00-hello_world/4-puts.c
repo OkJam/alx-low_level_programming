8d3859999dba0853eedfb0bda73943e1b83513b5
James Okwoche <jamesokwocheo@gmail.com>
2022-06-30 12:18:22 -0700

    add: 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#include<stdio.h>
+int main()
+{
+    //initialize the string
+    char c_programming[] = "Programming is like building a multilingual puzzle";
+    puts(c_programming);
+    return 0;
+}
