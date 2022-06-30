00c83106a23f53afd5e2b1d99c0d09ad4cc38f17
James Okwoche <jamesokwocheo@gmail.com>
2022-06-30 12:31:20 -0700

    update: 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #include<stdio.h>
-int main()
+int main(void)
 {
-    //initialize the string
-    char c_programming[] = "Programming is like building a multilingual puzzle";
-    puts(c_programming);
-    return 0;
+    /*
+     * Print string
+     */
+     puts("Programming is like building a multilingual puzzle");
+     return 0;
 }
