1abcbef9abcd526d6db71a5fba0ddbc19443134b
James Okwoche <jamesokwocheo@gmail.com>
2022-06-30 21:02:41 +0000

    Update: 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
-/* entry point */
+/*
+*entry point
+*/
 int main(void)
 {
-	/*
-	* print string
-	*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
