e282975b9a22fb01c8eaae30ae0d16d694541865
James Okwoche <jamesokwocheo@gmail.com>
2022-07-01 20:08:20 +0000

    update: 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,7 @@ int main(void)
 	printf("The size of a character is %d byte(s)\n", sizeof(char));
 	printf("The size of a integer  is %d byte(s)\n", sizeof(int));
 	printf("The size of a long integer is %d byte(s)\n", sizeof(long int));
+	printf("The size of a long long integer is %d byte(s)\n", sizeof(long long int));
+	printf("The size of a float is %d byte(s)\n", sizeof(float));
 	return (0);
 }
