aa20776d6e6d9d48707cbf6319474cde8eabb401
James Okwoche <jamesokwocheo@gmail.com>
2022-07-01 20:05:18 +0000

    add: 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Main - Entry Point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	printf("The size of a character is %d byte(s)\n", sizeof(char));
+	printf("The size of a integer  is %d byte(s)\n", sizeof(int));
+	printf("The size of a long integer is %d byte(s)\n", sizeof(long int));
+	return (0);
+}
