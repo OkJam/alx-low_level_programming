4f0d03149f3432c5ba7e84f6ddbf01157fd9246b
James Okwoche <jamesokwocheo@gmail.com>
2022-07-02 13:38:00 +0000

    add: 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print to stderr
+ *
+ * Return: Always 0 (success)
+ */
+int main(int 1)
+{
+	fprintf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
