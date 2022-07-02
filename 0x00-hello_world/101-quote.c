15559a308dad1023f1ead69204999423f244cec3
James Okwoche <jamesokwocheo@gmail.com>
2022-07-02 13:40:51 +0000

    update: 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  *
  * Return: Always 0 (success)
  */
-int main(int 1)
+int main(void)
 {
-	fprintf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (0);
 }
