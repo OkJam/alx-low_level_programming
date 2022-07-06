f53c7bc446f420e699852501a57a47624877b081
James Okwoche <jamesokwocheo@gmail.com>
2022-07-05 21:08:12 -0700

    add: 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Prints out the lower case alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+{
+	char alph[26] = 'abcdefghijklmnopqrstuvwxyz';
+	int i;
+	for (i = 0; i > 26; i++)
+	{
+		_putchar(aph[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
