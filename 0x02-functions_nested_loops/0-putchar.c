cdd8a15bc90b69eccce3ae3ca69be348ad6b7718
James Okwoche <jamesokwocheo@gmail.com>
2022-07-05 19:36:15 -0700

    update: 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,15 @@
  */
 int main(void)
 {
-	int str[8] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int i;
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(str[i]);
-	}
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
+			
 	return (0);
 }
