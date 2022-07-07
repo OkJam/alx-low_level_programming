87063743e7ead56028b9abec59da51d9c0dcee31
James Okwoche <jamesokwocheo@gmail.com>
2022-07-07 07:52:16 -0700

    edit files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main - tests functions for negetive or positive integers and 
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
