dada7b98475e3b009fe674a454a40b9743627623
James Okwoche <jamesokwocheo@gmail.com>
2022-07-06 19:46:36 -0700

    add: 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns
+ * the result.
+ *
+ * @i: first number
+ * @j: second number
+ *
+ * Return: sum of i and j.
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
