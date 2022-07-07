d113b4c5d3e0962a3bcb8cfa088f73e441d8e1a7
James Okwoche <jamesokwocheo@gmail.com>
2022-07-06 17:58:05 -0700

    update: 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,21 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * _islower - check whether a character is lower or not
+ * _islower - check if a character is lower letter or not
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 if lower,  0 if upper.
  */
 int _islower(int c)
 {
-	if (_islower(c) ==TRUE)
-		return 1;
-	return 0;
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
