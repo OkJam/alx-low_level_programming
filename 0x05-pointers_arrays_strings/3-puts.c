30f05cc54b4d7c0b561ce785a8a13d24514759be
James Okwoche <jamesokwocheo@gmail.com>
2022-07-13 04:05:07 -0700

    updated: 3-puts.c file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,17 +7,5 @@
  */
 void _puts(char *str)
 {
-	int count;
-
-	count = 0;
-	while (count >= 0)
-	{
-		if (str[count] == '\0')
-		{
-			_puts('\n');
-			break;
-		}
-		_puts(str[count]);
-		count++;
-	}
+	_puts(str)
 }
