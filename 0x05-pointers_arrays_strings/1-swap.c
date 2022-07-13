6a5cf79b20cf649e3da02b75c34aba0804649403
James Okwoche <jamesokwocheo@gmail.com>
2022-07-12 19:49:36 -0700

    added: 1-swap.c files

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: None
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
