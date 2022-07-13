bf36496a0aee1f0b3552915a8a7078f866ebbbd6
James Okwoche <jamesokwocheo@gmail.com>
2022-07-12 19:30:37 -0700

    added: 0-reset_to_98.c, main.h, README.md files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates pointer value to 98
+ *
+ * @n: Input integer
+ *
+ * Return: None
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#endif
+
+void reset_to_98(int *n);
