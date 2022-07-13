f8b7bf0af3b3cd6902c15c00a600820f9ace5037
James Okwoche <jamesokwocheo@gmail.com>
2022-07-13 00:56:21 -0700

    added: 3-puts.c file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#iclude "main.h"
+
+/**
+ * _puts - print a string to stdout
+ * @str: input string
+ * Return: None
+ */
+void _puts(char *str)
+{
+	int count;
+
+	count = 0;
+	for (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
