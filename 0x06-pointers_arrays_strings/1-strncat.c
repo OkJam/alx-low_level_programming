95e0b1d401ec6ebb41b1b0e0d0084e56449e6fd8
James Okwoche <jamesokwocheo@gmail.com>
2022-07-14 03:17:48 -0700

    added: 1-strncat.c file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings using at most n bytes
+ * @dest: destination file
+ * @src: source file
+ * Return: Pointer to dest file
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int strcount1 = 0, strcount2 = 0;
+
+	while (*(dest + strcount1) != '\0')
+	{
+		strcount1++;
+	}
+	while (strcount2 < n)
+	{
+		*(dest + strcount1) = *(src + strcount2);
+		if (*(src + strcount2) == '\0')
+		{
+			break;
+		}
+
+		strcount1++;
+		strcount2++;
+	}
+	return (dest);
+}
