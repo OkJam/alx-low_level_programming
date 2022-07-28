bf4d6d8e5b5f435628f8d078f28a04facaf1917e
James Okwoche <jamesokwocheo@gmail.com>
2022-07-28 01:26:00 -0700

    commited 3-mul.c file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two numbers
+ * @argc: number of command line arguments
+ * @argv: list of command line arguments
+ * Return: 1 if number of argument is not two
+ */
+
+int main(int argc, char *argv[])
+{
+	int min = atoi(argv[1]);
+	int max = atoi(argv[2]);
+
+	if ((argc - 1) != 2)
+	{
+		printf("Error\n");
+	}
+	else
+		printf("%d\n", min * max);
+	return (1);
+}
